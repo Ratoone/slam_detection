48fec737f44b1b80657e30918424e6d67e59a654
Ratoone <vacariu.paul@gmail.com>
2020-04-02 22:37:48 +0200

    Added calibration

---
+++ include/Calibration.h
@@ -0,0 +1,30 @@
+//
+// Created by ratoone on 02-04-20.
+//
+
+#ifndef SLAM_DETECTION_CALIBRATION_H
+#define SLAM_DETECTION_CALIBRATION_H
+
+
+#include <opencv2/core/mat.hpp>
+
+class Calibration {
+private:
+    cv::Size patternSize = cv::Size(4, 4);
+    cv::Mat cameraMatrix;
+    cv::Mat distortion;
+    cv::Mat newCameraMatrix;
+public:
+    explicit Calibration(const std::string &parameterFilePath, const std::string &calibrationImagePath = "");
+
+    void calibrate(const std::string& calibrationImagePath);
+
+    cv::Mat rectifyImage(const cv::Mat& image);
+
+    void saveCalibToFile(const std::string &filePath);
+
+    void loadCalibFromFile(const std::string &filePath);
+};
+
+
+#endif //SLAM_DETECTION_CALIBRATION_H
---
+++ src/Calibration.cpp
@@ -0,0 +1,76 @@
+//
+// Created by ratoone on 02-04-20.
+//
+
+#include <opencv2/opencv.hpp>
+#include "Calibration.h"
+#include <filesystem>
+
+Calibration::Calibration(const std::string& parameterFilePath, const std::string& calibrationImagePath) {
+    if (std::filesystem::exists(parameterFilePath)){
+        loadCalibFromFile(parameterFilePath);
+        return;
+    }
+
+    assert(!calibrationImagePath.empty());
+    calibrate(calibrationImagePath);
+    saveCalibToFile(parameterFilePath);
+}
+
+void Calibration::calibrate(const std::string& calibrationImagePath) {
+    std::vector<std::vector<cv::Point3f>> objectPoints;
+    std::vector<std::vector<cv::Point2f>> imagePoints;
+
+    std::vector<cv::Point3f> points;
+
+    for(int i = 0; i < patternSize.height; i++){
+        for(int j = 0; j < patternSize.width; j++) {
+            points.emplace_back(j, i, 0);
+        }
+    }
+
+    std::vector<cv::String> images;
+    cv::glob(calibrationImagePath, images);
+    std::vector<cv::Point2f> corners;
+    cv::Mat image;
+
+    for (auto &imagePath : images){
+        image = cv::imread(imagePath);
+        bool found = cv::findChessboardCorners(image, patternSize, corners);
+        if (!found) {
+            continue;
+        }
+
+        cv::Mat gray;
+        cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+        cv::cornerSubPix(gray, corners, cv::Size(5, 5), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.0001));
+        objectPoints.emplace_back(points);
+        imagePoints.emplace_back(corners);
+    }
+
+    cv::Mat R, T;
+    cv::calibrateCamera(objectPoints, imagePoints, cv::Size(image.rows, image.cols), cameraMatrix, distortion, R, T);
+    newCameraMatrix = cv::getOptimalNewCameraMatrix(cameraMatrix, distortion, image.size(), 0);
+}
+
+cv::Mat Calibration::rectifyImage(const cv::Mat &image) {
+    cv::Mat undistorted;
+    cv::undistort(image, undistorted, cameraMatrix, distortion, newCameraMatrix);
+    return undistorted;
+}
+
+void Calibration::saveCalibToFile(const std::string& filePath){
+    cv::FileStorage file(filePath, cv::FileStorage::WRITE);
+    file << "cameraMatrix" << cameraMatrix;
+    file << "newCameraMatrix" << newCameraMatrix;
+    file << "distortion" << distortion;
+    file.release();
+}
+
+void Calibration::loadCalibFromFile(const std::string& filePath){
+    cv::FileStorage file(filePath, cv::FileStorage::READ);
+    file["cameraMatrix"] >> cameraMatrix;
+    file["newCameraMatrix"] >> newCameraMatrix;
+    file["distortion"] >> distortion;
+    file.release();
+}
