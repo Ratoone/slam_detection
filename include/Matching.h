800c37a7653adff06ffadb0767b75515e2cdf27a
Ratoone <vacariu.paul@gmail.com>
2020-04-10 23:07:33 +0200

    4th attempt at point cloud generation

--- include/Matching.h
+++ include/Matching.h
@@ -11,14 +11,20 @@
 class Matching {
 private:
     cv::Mat cameraMatrix;
+    cv::Mat rotation = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat translation = cv::Mat::zeros(3, 1, CV_64F);
 public:
     explicit Matching(cv::Mat cameraMatrix);
 
     std::vector<cv::Point2f> featureDetection(const cv::Mat& image);
 
-    std::pair<cv::Mat, cv::Mat> featureMatching(const cv::Mat &source, const cv::Mat &target);
+    cv::Mat featureMatching(const cv::Mat &source, const cv::Mat &target);
 
     std::pair<cv::Mat, cv::Mat> findTransformation(std::vector<cv::Point2f> &sourcePoints, std::vector<cv::Point2f> &targetPoints);
+
+    [[nodiscard]] const cv::Mat &getRotation() const;
+
+    [[nodiscard]] const cv::Mat &getTranslation() const;
 };
 
 
--- src/Matching.cpp
+++ src/Matching.cpp
@@ -6,6 +6,8 @@
 #include <opencv2/opencv.hpp>
 #include <utility>
 #include "Matching.h"
+#define CERES_FOUND 1
+#include <opencv2/sfm/reconstruct.hpp>
 
 Matching::Matching(cv::Mat cameraMatrix) : cameraMatrix{std::move(cameraMatrix)}{}
 
@@ -18,33 +20,47 @@ std::vector<cv::Point2f> Matching::featureDetection(const cv::Mat& image) {
     return points;
 }
 
-std::pair<cv::Mat, cv::Mat> Matching::featureMatching(const cv::Mat& source, const cv::Mat& target){
+cv::Mat Matching::featureMatching(const cv::Mat& source, const cv::Mat& target){
     std::vector<float> error;
     auto sourcePoints = featureDetection(source);
     auto targetPoints = featureDetection(target);
     std::vector<uchar> status;
     cv::calcOpticalFlowPyrLK(source, target, sourcePoints, targetPoints, status, error);
 
-    int indexCorrection = 0;
-    for( int i=0; i<status.size(); i++){
-        cv::Point2f pt = targetPoints.at(i- indexCorrection);
-        if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
-            if((pt.x<0)||(pt.y<0))	{
-                status.at(i) = 0;
-            }
-            sourcePoints.erase (sourcePoints.begin() + i - indexCorrection);
-            targetPoints.erase (targetPoints.begin() + i - indexCorrection);
-            indexCorrection++;
-        }
+    auto transformation =  findTransformation(sourcePoints, targetPoints);
+    cv::Mat projectInitial = cameraMatrix * cv::Mat(cv::Matx34d(
+            rotation.at<double>(0,0), rotation.at<double>(0,1),rotation.at<double>(0,2),translation.at<double>(0),
+            rotation.at<double>(1,0), rotation.at<double>(1,1),rotation.at<double>(1,2),translation.at<double>(1),
+            rotation.at<double>(2,0), rotation.at<double>(2,1),rotation.at<double>(2,2),translation.at<double>(2)
+            ));
 
-    }
+    translation = translation + rotation * transformation.second;
+    rotation = transformation.first * rotation;
 
-    return findTransformation(sourcePoints, targetPoints);
+    cv::Mat projectFinal = cameraMatrix * cv::Mat(cv::Matx34d(
+            rotation.at<double>(0,0), rotation.at<double>(0,1),rotation.at<double>(0,2),translation.at<double>(0),
+            rotation.at<double>(1,0), rotation.at<double>(1,1),rotation.at<double>(1,2),translation.at<double>(1),
+            rotation.at<double>(2,0), rotation.at<double>(2,1),rotation.at<double>(2,2),translation.at<double>(2)
+    ));
+
+    cv::Mat points4D, points3D;
+    cv::triangulatePoints(projectInitial, projectFinal, sourcePoints, targetPoints, points4D);
+    cv::Mat points4DChanneled = points4D.reshape(4,1);
+    cv::convertPointsFromHomogeneous(points4DChanneled, points3D);
+    return points3D;
 }
 
 std::pair<cv::Mat, cv::Mat> Matching::findTransformation(std::vector<cv::Point2f>& sourcePoints, std::vector<cv::Point2f>& targetPoints){
     auto essentialMatrix = cv::findEssentialMat(sourcePoints, targetPoints, cameraMatrix);
-    cv::Mat rotation, translation;
-    cv::recoverPose(essentialMatrix, sourcePoints, targetPoints, cameraMatrix, rotation, translation);
-    return {rotation, translation};
+    cv::Mat relRotation, relTranslation;
+    cv::recoverPose(essentialMatrix, sourcePoints, targetPoints, cameraMatrix, relRotation, relTranslation);
+    return {relRotation, relTranslation};
+}
+
+const cv::Mat &Matching::getRotation() const {
+    return rotation;
+}
+
+const cv::Mat &Matching::getTranslation() const {
+    return translation;
 }
