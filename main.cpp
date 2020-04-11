ed2f28cbb0d677838157c0ce9cdae071995a4de8
Ratoone <vacariu.paul@gmail.com>
2020-04-11 21:05:16 +0200

    Write output video to file

--- main.cpp
+++ main.cpp
@@ -14,8 +14,13 @@ int main(){
     VideoStream video = VideoStream("../images/MVI_3820.MP4");
     Matching matching(calib.getCameraMatrix().clone());
     ObjectDetection objectDetection;
+    auto videoWriter = cv::VideoWriter("../images/output.MP4",cv::VideoWriter::fourcc('M','P','4','V'),25,cv::Size(1400,600));
 
     cv::Mat trajectory = cv::Mat::zeros(600, 600, CV_8UC3);
+    cv::arrowedLine(trajectory,cv::Point(10, 500), cv::Point(10, 550), CV_RGB(255, 255, 255));
+    cv::putText(trajectory, "Z", cv::Point(20, 550), cv::FONT_HERSHEY_PLAIN,1,CV_RGB(255,255,255));
+    cv::arrowedLine(trajectory,cv::Point(10, 500), cv::Point(60, 500), CV_RGB(255, 255, 255));
+    cv::putText(trajectory, "X", cv::Point(70, 500), cv::FONT_HERSHEY_PLAIN,1,CV_RGB(255,255,255));
 
     cv::Mat frame = video.getNextFrame().value();
     std::optional<cv::Mat> next = video.getNextFrame();
@@ -24,21 +29,21 @@ int main(){
         frame = calib.rectifyImage(frame);
         cv::Mat nextFrame = calib.rectifyImage(next.value());
 
-        auto pointCloud = matching.featureMatching(frame, nextFrame);
+        matching.featureMatching(frame, nextFrame);
         auto rotation = matching.getRotation();
         auto translation = matching.getTranslation();
 
-        std::vector<cv::Vec3b> displayColor;
-        for (int i = 0; i < pointCloud.rows; i++){
-            auto point = pointCloud.at<cv::Vec3f>(i);
-            displayPointCloud.push_back(point);
-        }
+        objectDetection.detectImage(frame);
 
         int x = int(translation.at<double>(0)) + 300;
-        int y = int(translation.at<double>(2)) + 100;
+        int y = int(translation.at<double>(2)) + 300;
         cv::circle(trajectory, cv::Point(x, y) ,1, CV_RGB(255,0,0), 2);
+        char *coordinates = new char[255];
+        sprintf(coordinates, "X: %.2f, Y: %.2f, Z: %.2f",translation.at<double>(0),translation.at<double>(1), translation.at<double>(2));
+        cv::rectangle(trajectory, cv::Point(0,0), cv::Point(trajectory.cols,100),CV_RGB(0,0,255), -1);
+        cv::putText(trajectory, coordinates, cv::Point(10, 50), cv::FONT_HERSHEY_PLAIN,1,CV_RGB(255,255,255));
 
-        objectDetection.detectImage(frame);
+        // merge the two images
         cv::Mat combinedFrame(std::max(frame.rows, trajectory.rows), frame.cols + trajectory.cols, CV_8UC3);
         cv::Mat leftFragment(combinedFrame, cv::Rect(0, 0, frame.size().width, frame.size().height));
         frame.copyTo(leftFragment);
@@ -47,15 +52,8 @@ int main(){
 
         cv::imshow("", combinedFrame);
         cv::waitKey(1);
+        videoWriter.write(combinedFrame);
         frame = next.value();
         next = video.getNextFrame();
     }
-
-    cv::viz::Viz3d window("PCL");
-    window.setWindowSize(cv::Size(500, 500));
-    window.setWindowPosition(cv::Point(150, 150));
-
-    cv::viz::WCloud cloud(displayPointCloud);
-    window.showWidget("123",cloud);
-    window.spin();
 }
